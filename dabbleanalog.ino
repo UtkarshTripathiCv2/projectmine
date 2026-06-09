b6296481c90286add4a6048da5e0f9162919a7f6
Utkarsh Tripathi <145178226+UtkarshTripathiCv2@users.noreply.github.com>
2026-06-09 15:51:01 +0530

    Create dabbleanalog.ino

---
+++ dabbleanalog.ino
@@ -0,0 +1,90 @@
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
+#include <DabbleESP32.h>
+
+// --- Right Motor Pins ---
+const int enableRightMotor = 22; 
+const int rightMotorPin1 = 16;
+const int rightMotorPin2 = 17;
+
+// --- Left Motor Pins ---
+const int enableLeftMotor = 23;
+const int leftMotorPin1 = 18;
+const int leftMotorPin2 = 19;
+
+#define MAX_MOTOR_SPEED 255
+
+void rotateMotor(int rightMotorSpeed, int leftMotorSpeed) {
+  // --- Right Motor Direction ---
+  if (rightMotorSpeed < 0) {
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, HIGH);    
+  } else if (rightMotorSpeed > 0) {
+    digitalWrite(rightMotorPin1, HIGH);
+    digitalWrite(rightMotorPin2, LOW);      
+  } else {
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, LOW);      
+  }
+  
+  // --- Left Motor Direction ---
+  if (leftMotorSpeed < 0) {
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, HIGH);    
+  } else if (leftMotorSpeed > 0) {
+    digitalWrite(leftMotorPin1, HIGH);
+    digitalWrite(leftMotorPin2, LOW);      
+  } else {
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, LOW);      
+  }
+  
+  // --- Apply Speed using analogWrite ---
+  analogWrite(enableRightMotor, abs(rightMotorSpeed));
+  analogWrite(enableLeftMotor, abs(leftMotorSpeed));  
+}
+
+void setup() {
+  // Set all motor pins as standard outputs
+  pinMode(enableRightMotor, OUTPUT);
+  pinMode(rightMotorPin1, OUTPUT);
+  pinMode(rightMotorPin2, OUTPUT);
+  
+  pinMode(enableLeftMotor, OUTPUT);
+  pinMode(leftMotorPin1, OUTPUT);
+  pinMode(leftMotorPin2, OUTPUT);
+
+  // Ensure motors are stopped on startup
+  rotateMotor(0, 0); 
+  
+  // Start Bluetooth connection
+  Dabble.begin("MyBluetoothCar"); 
+}
+
+void loop() {
+  int rightMotorSpeed = 0;
+  int leftMotorSpeed = 0;
+  
+  Dabble.processInput(); // Read Bluetooth data from phone
+  
+  // Determine speed and direction based on buttons
+  if (GamePad.isUpPressed()) {
+    rightMotorSpeed = MAX_MOTOR_SPEED;
+    leftMotorSpeed = MAX_MOTOR_SPEED;
+  }
+  else if (GamePad.isDownPressed()) {
+    rightMotorSpeed = -MAX_MOTOR_SPEED;
+    leftMotorSpeed = -MAX_MOTOR_SPEED;
+  }
+  else if (GamePad.isLeftPressed()) {
+    rightMotorSpeed = MAX_MOTOR_SPEED;
+    leftMotorSpeed = -MAX_MOTOR_SPEED;
+  }
+  else if (GamePad.isRightPressed()) {
+    rightMotorSpeed = -MAX_MOTOR_SPEED;
+    leftMotorSpeed = MAX_MOTOR_SPEED;
+  }
+
+  // Send the final commands to the motors
+  rotateMotor(rightMotorSpeed, leftMotorSpeed);
+}
